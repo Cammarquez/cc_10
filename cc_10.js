957c62ef3b9efed5567403f37dbf0e22412f16d7
Cammarquez <cameronmarquez@usf.edu>
2025-02-23 14:08:54 -0500

    Task 2 - Created Order Class

--- cc_10.js
+++ cc_10.js
@@ -22,3 +22,25 @@ class Product {
 const prod1 = new Product('Laptop',101,1200,10); //creates a new object of the class Product
 console.log(prod1.getDetails()); //prints the details of the product
 console.log(prod1.updateStock(5)); //updates the stock of the product and prints the updated stock
+
+//Task 2
+class Order {
+    constructor(orderId, product, quantity) //creates a class with 3 parameters
+    {
+        this.orderId = orderId;
+        this.product = product;
+        this.quantity = quantity;
+        if (this.product.stock >= this.quantity) {
+            this.product.stock -= this.quantity; //updates the stock of the product when an order is placed (when an object is created in the product class)
+        } else {
+            throw new Error('Insufficient stock to place the order');
+        }
+    }
+        getOrderDetails() //creates a method to get the details of the order
+    {
+            return `Ordered Product: ${this.product.name}, Ordered Quantity: ${this.quantity}, Order ID: ${this.orderId}`; //returns the details of the order
+    }
+}
+const order1 = new Order(501, prod1, 2); //creates a new object of the class Order
+console.log(order1.getOrderDetails()); //prints the details of the order
+console.log(prod1.getDetails()); //prints the updated stock of the product
